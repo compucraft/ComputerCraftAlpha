11c9a1f0290d1746da6a074d3dfd6e1cc5fc7279
Daniel Sapelli <daniel.sapelli@gmail.com>
2012-05-07 00:32:20 -0400

    NuclearReactorControlCenter First Commit

---
+++ NRCC.lua
@@ -0,0 +1,14 @@
+function display()
+term.clear()
+term.setCursorPos(1,1)
+end
+copyright = "Advtech (c) 2012, All Rights Reserved"
+debug = "pie"
+display()
+print ("Welcome to NRCC")
+sleep(0.25)
+print ("Helping you maintain your reactor")
+sleep(0.25)
+print ("A BluSoft Product")
+display()
+print("Running NRCC 1.0")
